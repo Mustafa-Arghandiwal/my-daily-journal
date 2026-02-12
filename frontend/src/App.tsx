31aa54d0aa98674ecbaf00acb0a592b60b9e9c3b
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-02-12 10:54:22 +0430

    Initialize frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
 
 function App() {
 
+	const [data, setData] = useState("")
+	function clickHandler() {
+		fetch('http://localhost:3000/msg')
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(`Server error: ${response.status} ${response.statusText}`)
+				}
+				return response.json()
+			})
+			.then(data => {
+				console.log(data)
+				setData(data.msg)
+			}
+			)
+			.catch(err => console.log(err))
+	}
+
 	return (
-		<>Hi there</>
+		<>
+			<button className="temp" onClick={clickHandler}>
+				Click me
+			</button>
+			<hr />
+			<div className="temp">{data}</div>
+		</>
 	)
 }
 
