4f83003fc47f1bdaed29e2711cbafbdd9ef802d8
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-02-17 15:58:19 +0430

    nothing special

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,9 +20,6 @@ function App() {
 	}
 
 	const [info, setInfo] = useState({ email: "", password: "" })
-	const handleSubmit = (e) => {
-		e.preventDefault()
-	}
 
 
 	return (
@@ -35,10 +32,22 @@ function App() {
 			<div className="temp">{data}</div>
 
 			{info.email} {info.password}
-			<form className="flex flex-col gap-8 mt-10 max-w-100 p-8">
+			<form onSubmit={(e) => {
+				e.preventDefault()
+				fetch('http://localhost:3000/register', {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(info)
+				}).then(response => response.json())
+					.then(data => { setData(data.message) })
+
+			}}
+				className="flex flex-col gap-8 mt-10 max-w-100 p-8">
 				<input className="border rounded-sm p-1" type="text" name="email" placeholder="Email" onChange={(e) => setInfo({ ...info, email: e.target.value })} value={info.email} />
 				<input className="border rounded-sm p-1" type="password" name="password" placeholder="Password" onChange={(e) => setInfo({ ...info, password: e.target.value })} value={info.password} />
-				<button className="border rounded-sm p-1" type="submit" onClick={(e) => handleSubmit(e)}>Sign in</button>
+				<button className="border rounded-sm p-1" type="submit">Sign in</button>
 			</form>
 		</>
 	)
