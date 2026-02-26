8cfef14bc69c49c1809f7332e75670b8dd517015
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-02-26 13:59:46 +0430

    Setup form data post to backend

--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -1,4 +1,6 @@
 import { X } from "lucide-react";
+import { useState } from "react";
+// import { useState } from "react";
 
 type Props = {
 	isSignUpModalOpen: boolean,
@@ -8,32 +10,59 @@ export default function SignUpModal({ isSignUpModalOpen, setIsSignUpModalOpen }:
 
 	if (!isSignUpModalOpen) return null
 
+	const [msg, setMsg] = useState('')
+
+	const signUp = async (formData: FormData) => {
+		const signUpData = Object.fromEntries(formData)
+		try {
+			const res = await fetch('http://localhost:3000/auth/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(signUpData)
+			})
+
+			const result = await res.json()
+			if (!res.ok) {
+				setMsg(result.error)
+			} else {
+				setMsg(result.message)
+			}
+		} catch (err) {
+			console.log(err)
+		}
+
+	}
 	return (
-		<form className=" bg-white p-8 absolute top-36 left-1/2 -translate-x-1/2 border-2 rounded-md flex flex-col gap-2">
+		<form action={signUp} className=" bg-white p-8 absolute top-36 left-1/2 -translate-x-1/2 border-2 rounded-md flex flex-col gap-2">
 
 			<button className="absolute cursor-pointer right-2 top-2" onClick={() => setIsSignUpModalOpen(false)}>
 				<X />
 			</button>
-			<h3 className="font-bold text-3xl text-center">Sign Up</h3>
+
+
+			<h3 className="font-bold text-3xl text-center">Sign Up </h3>
 
 			<label className="block ">
 				Name
-				<input type="text" className="block px-1 border rounded-md  min-w-64 " />
+				<input type="text" name="name" className="block px-1 border rounded-md  min-w-64 " />
 			</label>
 			<label className="block  ">
 				Email
-				<input type="text" className="block px-1 border rounded-md w-full " />
+				<input type="text" name="email" className="block px-1 border rounded-md w-full " />
 			</label>
 			<label className="block  ">
 				Password
-				<input type="password" className="block px-1 border rounded-md w-full" />
+				<input type="password" name="password" className="block px-1 border rounded-md w-full" />
 			</label>
 			<label className="block  ">
 				Confirm Password
-				<input type="password" className="block px-1 border rounded-md w-full " />
+				<input type="password" name="confirmPassword" className="block px-1 border rounded-md w-full " />
 			</label>
 
-			<button onClick={(e) => e.preventDefault()} className="font-bold w-full  mt-5 cursor-pointer py-1 rounded-md text-white bg-black">Sign Up</button>
+			<button className="font-bold w-full  mt-5 cursor-pointer py-1 rounded-md text-white bg-black">Sign Up</button>
+			<p className="text-center text-green-500">{msg}</p>
 
 		</form>
 	)
