681745ea28a77531334f6cc28e3ed8fdf75d911c
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-03-04 10:04:04 +0430

    Implement sign up

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,33 +2,52 @@ import { Router } from "express"
 const router = Router()
 import db from "../db";
 import bcrypt from 'bcrypt'
+import z from "zod";
 
 
 router.post('/register', async (req, res) => {
 	try {
-		const { name, email, password, confirmPassword } = req.body
-		if (!name || !email || !password || !confirmPassword) {
-			return res.status(400).json({ error: 'All fields are required.' })
+
+		const UserSchema = z.object({
+			name: z.string().min(5),
+			email: z.email(),
+			password: z.string().min(6),
+			confirmPassword: z.string()
+		}).refine(data => data.password === data.confirmPassword, {
+			path: ["confirmPassword"],
+			message: 'Passwords do not match'
+		})
+
+		const result = UserSchema.safeParse(req.body)
+		if (!result.success) {
+
+			let errorsObject: Record<string, string> = {}
+			result.error.issues.forEach(issue => {
+				const key = issue.path[0] ?? "unknown"
+				errorsObject[String(key)] = issue.message
+			})
+			return res.status(400).json({ errors: errorsObject })
+			// errors: result.error.issues.map(issue => {
+			// 	return { [String(issue.path[0])]: issue.message }
+			// })
 		}
 
+		const { name, email, password } = result.data
+
+
 		const existingUser = db.prepare('SELECT * FROM users WHERE email = ?').get(email)
-		// console.log(existingUser)//object of user
+
 		if (existingUser) {
 			return res.status(400).json({ error: 'Email already registered.' })
 		}
 
+
 		const hashedPassword = await bcrypt.hash(password, 10)
 		const statement = db.prepare('INSERT INTO users (name, email, password) VALUES (?, ?, ?)')
 		statement.run(name, email, hashedPassword)
-		return res.status(201).json({
-			message: "User registered successfully!"
-		})
-
-
-		res.json({ message: 'registered!' })
+		return res.status(201).json({ message: 'User registered successfully.' })
 	} catch (err) {
 		console.log(err)
-
 	}
 })
 
--- frontend/src/components/SignUpModal.tsx
+++ frontend/src/components/SignUpModal.tsx
@@ -1,19 +1,40 @@
 import { X } from "lucide-react";
 import { useState } from "react";
-// import { useState } from "react";
+import { useFormStatus } from "react-dom";
+
 
 type Props = {
 	isSignUpModalOpen: boolean,
 	setIsSignUpModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
+type ErrorType = {
+	name?: string,
+	email?: string,
+	password?: string,
+	confirmPassword?: string
+}
+
+function SubmitButton() {
+	const { pending } = useFormStatus()
+	return <button disabled={pending} className="font-bold w-full  mt-5 cursor-pointer py-1 rounded-md text-white bg-black disabled:bg-gray-300 ">
+		{pending ? "Submitting..." : "Submit"}
+	</button>
+}
+
 export default function SignUpModal({ isSignUpModalOpen, setIsSignUpModalOpen }: Props) {
 
 	if (!isSignUpModalOpen) return null
 
 	const [msg, setMsg] = useState('')
+	const [errors, setErrors] = useState<ErrorType>({})
 
-	const signUp = async (formData: FormData) => {
+	const signUp = async (e: React.SubmitEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const form = e.currentTarget
+
+		const formData = new FormData(form)
 		const signUpData = Object.fromEntries(formData)
+		// await new Promise(resolve => setTimeout(resolve, 2000))
 		try {
 			const res = await fetch('http://localhost:3000/auth/register', {
 				method: 'POST',
@@ -25,9 +46,11 @@ export default function SignUpModal({ isSignUpModalOpen, setIsSignUpModalOpen }:
 
 			const result = await res.json()
 			if (!res.ok) {
-				setMsg(result.error)
+				setErrors(result.errors)
 			} else {
 				setMsg(result.message)
+				form.reset()
+				setErrors({})
 			}
 		} catch (err) {
 			console.log(err)
@@ -35,7 +58,7 @@ export default function SignUpModal({ isSignUpModalOpen, setIsSignUpModalOpen }:
 
 	}
 	return (
-		<form action={signUp} className=" bg-white p-8 absolute top-36 left-1/2 -translate-x-1/2 border-2 rounded-md flex flex-col gap-2">
+		<form onSubmit={signUp} className=" bg-white p-8 absolute top-36 left-1/2 -translate-x-1/2 border-2 rounded-md flex flex-col gap-2 w-72 sm:w-96">
 
 			<button className="absolute cursor-pointer right-2 top-2" onClick={() => setIsSignUpModalOpen(false)}>
 				<X />
@@ -46,22 +69,27 @@ export default function SignUpModal({ isSignUpModalOpen, setIsSignUpModalOpen }:
 
 			<label className="block ">
 				Name
-				<input type="text" name="name" className="block px-1 border rounded-md  min-w-64 " />
+				<input type="text" name="name" className="block px-1 border rounded-md  w-full " />
+				<p className="text-sm text-red-600 ">{errors.name || null}</p>
 			</label>
 			<label className="block  ">
 				Email
 				<input type="text" name="email" className="block px-1 border rounded-md w-full " />
+				<p className="text-sm text-red-600 ">{errors.email || null}</p>
 			</label>
 			<label className="block  ">
 				Password
 				<input type="password" name="password" className="block px-1 border rounded-md w-full" />
+				<p className="text-sm text-red-600 ">{errors.password || null}</p>
 			</label>
 			<label className="block  ">
 				Confirm Password
 				<input type="password" name="confirmPassword" className="block px-1 border rounded-md w-full " />
+				<p className="text-sm text-red-600 ">{errors.confirmPassword || null}</p>
 			</label>
 
-			<button className="font-bold w-full  mt-5 cursor-pointer py-1 rounded-md text-white bg-black">Sign Up</button>
+			{/* <button className="font-bold w-full  mt-5 cursor-pointer py-1 rounded-md text-white bg-black">Sign Up</button> */}
+			<SubmitButton />
 			<p className="text-center text-green-500">{msg}</p>
 
 		</form>
