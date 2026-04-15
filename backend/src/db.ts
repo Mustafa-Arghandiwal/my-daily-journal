5fa06d9d06625b50acf5bf46d4768b54fd7ad281
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-04-15 16:21:52 +0430

    Add entries table query

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,8 +2,9 @@ import Database from "better-sqlite3";
 
 const db = new Database('journal.db')
 db.pragma('journal_mode = WAL');
+db.pragma('foreign_keys = ON');
 
-const query = `
+const usersTableQuery = `
 	CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY,
 		name TEXT NOT NULL,
@@ -12,7 +13,21 @@ const query = `
 		created_at TEXT DEFAULT (datetime('now'))
 );
 `
-db.exec(query)
+const entriesTableQuery = `
+    CREATE TABLE IF NOT EXISTS entries (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    feeling TEXT,
+    content TEXT NOT NULL,
+    created_at TEXT DEFAULT (datetime('now')),
+    updated_at TEXT,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+`
+db.exec(usersTableQuery)
+db.exec(entriesTableQuery)
 
 console.log('Database READY!!!')
 export default db
