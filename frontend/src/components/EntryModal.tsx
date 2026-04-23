ef07589972bf3376548bd4e0655f309a3427578f
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-04-23 10:40:52 +0430

    Wrap all modals in <Portal>

--- frontend/src/components/EntryModal.tsx
+++ frontend/src/components/EntryModal.tsx
@@ -1,6 +1,7 @@
 import { X } from "lucide-react"
 import { useState } from "react"
 import confetti from 'canvas-confetti'
+import Portal from "./Portal"
 
 type Props = {
     isModalOpen: boolean,
@@ -67,39 +68,42 @@ export default function EntryModal({ isModalOpen, setIsModalOpen, refreshEntries
 
 
     return (
-        <form onSubmit={createEntry} className="absolute max-w-2xl w-full min-h-72 top-22 right-1/2 translate-x-1/2 bg-white border-2 p-6 rounded-md flex flex-col gap-4 ">
+        <Portal>
+            <form onSubmit={createEntry} className="relative w-[80vw] max-w-2xl min-h-72 bg-white border-2 p-6 rounded-md flex flex-col gap-4 ">
 
-            <button type="button" className="absolute cursor-pointer right-2 top-2"
-                onClick={() => setIsModalOpen(false)}
-            >
-                <X />
-            </button>
+                <button type="button" className="absolute cursor-pointer right-2 top-2"
+                    onClick={() => setIsModalOpen(false)}
+                >
+                    <X />
+                </button>
 
-            <input type="text" name="title" placeholder="Title"
-                className="mx-auto border-[0.5px] rounded-md text-4xl p-1 text-center " />
-            <p className="text-sm text-red-600 ">{errors.title}</p>
+                <input type="text" name="title" placeholder="Title"
+                    className="mx-auto border-[0.5px] rounded-md text-4xl p-1 text-center " />
+                <p className="text-sm text-red-600 ">{errors.title}</p>
 
-            <div className="text-center">
-                <p className="text-2xl mt-2">Feeling</p>
-                <input type="text" name="feeling" placeholder={`e.g. ${feelingPlaceholders[randomIndex]}`}
-                    className="p-1 mx-auto text-2xl border-[0.5px] rounded-md text-center  " />
-                <p className="text-sm text-red-600 ">{errors.feeling}</p>
-            </div>
+                <div className="text-center">
+                    <p className="text-2xl mt-2">Feeling</p>
+                    <input type="text" name="feeling" placeholder={`e.g. ${feelingPlaceholders[randomIndex]}`}
+                        className="p-1 mx-auto text-2xl border-[0.5px] rounded-md text-center  " />
+                    <p className="text-sm text-red-600">{errors.feeling}</p>
+                </div>
 
-            <textarea
-                name="content"
-                placeholder="Start writing here..."
-                onInput={(e) => {
-                    const target = e.currentTarget
-                    target.style.height = "auto"
-                    target.style.height = target.scrollHeight + "px"
-                }}
-                className="p-2 border rounded-md w-full min-h-52 resize-none overflow-hidden"
-            />
-            <p className="text-sm text-red-600">{errors.content}</p>
+                <textarea
+                    name="content"
+                    placeholder="Start writing here..."
+                    onInput={(e) => {
+                        const target = e.currentTarget
+                        target.style.height = "auto"
+                        target.style.height = target.scrollHeight + "px"
+                    }}
+                    className="p-2 border rounded-md w-full min-h-52 resize-none overflow-hidden"
+                />
+                <p className="text-sm text-red-600">{errors.content}</p>
 
-            <button className="rounded-md p-2 text-white bg-black w-fit ml-auto cursor-pointer">Save Entry</button>
+                <button className="rounded-md p-2 text-white bg-black w-fit ml-auto cursor-pointer">Save Entry</button>
 
-        </form>
+            </form>
+        </Portal>
+
     )
 }
